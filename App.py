00a9e0b1ca8a42a2680dc8b0e4584a873a15a906
Ted4523 <114703222+Ted4523@users.noreply.github.com>
2024-02-23 19:02:27 -0800

    modern look with ttkbootstrap

--- App.py
+++ App.py
@@ -1,28 +1,41 @@
-from time import strftime
 
 import requests
 import tkinter as tk
 from tkinter import messagebox
 from datetime import datetime
-from datetime import datetime, tzinfo
 from dateutil import tz
+import ttkbootstrap as ttk
 
-root = tk.Tk()
-root.title("Weather App")
-city_label = tk.Label(root, text="City:")
-city_label.pack()
-city_entry = tk.Entry(root)
-city_entry.pack()
-fetch_button = tk.Button()
+app = ttk.Window(themename="superhero")
+app.geometry("600x500")
+label = ttk.Label(app, text="Weather App")
+label.pack(pady=30)
+label.config(font=("Helvetica",20, "bold"))
+app.title("Weather App")
+cityframe = ttk.Frame(app)
+cityframe.pack(pady=15, padx=10, fill="x")
+ttk.Label(cityframe, text="City:").pack(side="left", padx=5)
+cname = ttk.Entry(cityframe)
+cname.pack(side="left", fill="x", expand=True, padx=5)
+button_frame = ttk.Frame(app)
+button_frame.pack(pady=50, padx=10, fill="x")
+fetch = ttk.Button(button_frame, text="Submit", bootstyle="success")
+fetch.pack(side="left", padx=10)
+ttk.Button(button_frame, text="Cancel", bootstyle="danger").pack(side="left", padx=10)
 
-fetch_button = tk.Button(root,text="Fetch Weather")
-fetch_button.pack()
-
-weather_label = tk.Label(root, text= "")
+weather_label = ttk.Label(app, text="")
 weather_label.pack()
 
+
+
+
+
+
+
+
+
 def fetch_weather():
-    city = city_entry.get()
+    city = cname.get()
     api_key = "ccdf6e3a31e9acb25a240c006b0b0645"
     url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}"
     try:
@@ -37,12 +50,14 @@ def fetch_weather():
         utc = utc.replace(tzinfo=tz.gettz('UTC'))
         local = utc.astimezone(tz.tzlocal())
         weather = data["weather"][0]["description"]
-        weather_label.config(text=f"Temperature: {temperature}°C\nWeather: {weather}\nWind Speed: {wind_speed}\nSunset: {local.hour}:{local.minute}")
-    except Exception as e:
+        weather_label.config(
+            text=f"Temperature: {temperature}°C\nWeather: {weather}\n"
+                 f"Wind Speed: {wind_speed}\nSunset: {local.hour}:{local.minute}\nHumidity: {humidity}")
+    except Exception:
         messagebox.showerror("Error", "Unable to fetch weather data")
 
 
-fetch_button.config(command=fetch_weather)
+fetch.config(command=fetch_weather)
 
 # Start the GUI main loop
-root.mainloop()
+app.mainloop()
